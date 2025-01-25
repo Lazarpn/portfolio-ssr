c3c3028967743211add7a09a60db23551d2f6116
Lazar <lazarst.pn@gmail.com>
2025-01-25 15:55:13 +0100

    Spacing fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'pt-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,13 +4,12 @@ import { AboutMeComponent } from '../about-me/about-me.component';
 import { ContactComponent } from '../contact/contact.component';
 import { ExperienceComponent } from '../experience/experience.component';
 import { ExpertiseComponent } from '../expertise/expertise.component';
-import { FooterComponent } from '../footer/footer.component';
 import { HeroComponent } from '../hero/hero.component';
 import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'pt-home',
-  imports: [HeroComponent, AboutMeComponent, ExpertiseComponent, ProjectsComponent, ExperienceComponent, ContactComponent, FooterComponent],
+  imports: [HeroComponent, AboutMeComponent, ExpertiseComponent, ProjectsComponent, ExperienceComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
