94699497a27ac893b236be934c9c45811948f4cc
Lazar <lazarst.pn@gmail.com>
2025-01-26 13:40:44 +0100

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 
 @Component({
   selector: 'pt-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  showLoadingSpinner = true;
+
+  constructor(private zone: NgZone) { }
+
+  ngOnInit(): void {
+    this.zone.onStable.subscribe(() => {
+      this.showLoadingSpinner = false;
+    });
+  }
 }
